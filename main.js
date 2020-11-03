2ae6acffb03e7a2841e834e079a8b121ce2ddfca
BluePR0 <waaromnietgg@gmail.com>
2020-11-03 20:05:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,19 +2,17 @@
 const fetch = require('node-fetch');
 const client = new Discord.Client();
 const prefix = '!'
-client.login('NzczMjQ1OTQ2OTAwNjQzODYx.X6GbZw.Wn0zVIR5TSBasJNqkGREGI6rO_w');
+client.login(process.env.token);
 
 client.on('ready', () => {
     console.log("Bot is online.");
 })
 
 client.on('message', async message => {
-    if (!message.content.startsWith(prefix) || message.author.bot)
-    {
+    if (!message.content.startsWith(prefix) || message.author.bot) {
         setTimeout(() => { message.delete(); console.log('Delete Message') }, 5000);
     }
-    else
-    {
+    else {
         setTimeout(() => { message.delete(); console.log('Delete Message') }, 5000);
     }
 
