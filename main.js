00a632db96a8d3d0866e444c9a5bfecc39d54fc4
BluePR0 <waaromnietgg@gmail.com>
2020-11-03 19:50:21 +0100

    .
    .

---
+++ main.js
@@ -0,0 +1,24 @@
+﻿const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const client = new Discord.Client();
+const prefix = '!'
+client.login('NzczMjQ1OTQ2OTAwNjQzODYx.X6GbZw.Wn0zVIR5TSBasJNqkGREGI6rO_w');
+
+client.on('ready', () => {
+    console.log("Bot is online.");
+})
+
+client.on('message', async message => {
+    if (!message.content.startsWith(prefix) || message.author.bot)
+    {
+        setTimeout(() => { message.delete(); console.log('Delete Message') }, 5000);
+    }
+    else
+    {
+        setTimeout(() => { message.delete(); console.log('Delete Message') }, 5000);
+    }
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+});
+
