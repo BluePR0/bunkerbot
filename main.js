be654239917c7062383f5fddcbccd728fc24cfad
BluePR0 <73907007+BluePR0@users.noreply.github.com>
2021-01-12 09:12:17 +0100

    Update main.js
    Changed time to delete from 5 sec. to 3. sec.

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ client.on('ready', () => {
 client.on('message', async message => {
     if (message.channel.id == "798463800730583091") {
         if (!message.content.startsWith(prefix) || message.author.bot) {
-            setTimeout(() => { message.delete(); console.log('Delete Message') }, 5000);
+            setTimeout(() => { message.delete(); console.log('Delete Message') }, 3000);
         }
         else {
-            setTimeout(() => { message.delete(); console.log('Delete Message') }, 5000);
+            setTimeout(() => { message.delete(); console.log('Delete Message') }, 3000);
         }
 
         const args = message.content.slice(prefix.length).split(/ +/);
